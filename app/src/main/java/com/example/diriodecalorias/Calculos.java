806e9f145294f328f0da9de4ddc8ce5a8bf41cef
lean-dro <72211521+lean-dro@users.noreply.github.com>
2021-06-23 20:11:57 -0300

    Spinner da cálculos 1

--- app/src/main/java/com/example/diriodecalorias/Calculos.java
+++ app/src/main/java/com/example/diriodecalorias/Calculos.java
@@ -3,12 +3,20 @@ package com.example.diriodecalorias;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Calculos extends AppCompatActivity {
-
+Spinner spFatores;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculos);
+
+        spFatores = (Spinner) findViewById(R.id.spFatores);
+
+        ArrayAdapter adapter  = ArrayAdapter.createFromResource(this, R.array.fatores, android.R.layout.simple_dropdown_item_1line);
+
+        spFatores.setAdapter(adapter);
     }
 }
